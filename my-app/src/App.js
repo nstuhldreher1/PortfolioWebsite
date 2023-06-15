5b96d81f44ce2ec7391f06486137d33a4ef882c7
nstuhldreher1 <nbstuhldreher@gmail.com>
2023-06-14 21:03:30 -0400

    Added all of the required files for a React App.

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import PortfolioHeader from './components/PortfolioHeader';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <PortfolioHeader/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/PortfolioHeader.js
@@ -0,0 +1,7 @@
+
+function PortfolioHeader(){
+    return(
+        <h1>Hello</h1>
+    );
+}
+export default PortfolioHeader;
