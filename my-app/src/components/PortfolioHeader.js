88dfe4f949ec04f42e19396f98f1fdec0fef4d1a
nstuhldreher1 <nbstuhldreher@gmail.com>
2023-06-14 22:38:13 -0400

    Added the header for the website.

--- my-app/src/components/PortfolioHeader.js
+++ my-app/src/components/PortfolioHeader.js
@@ -1,7 +1,12 @@
+import './PortfolioHeader.css';
 
 function PortfolioHeader(){
     return(
-        <h1>Hello</h1>
+        <div id="header">
+            <h1 id="title">Hey, my name is Nick Stuhldreher.</h1>
+            <p id="description"> Welcome to my portfolio website. I'm a computer science student with a passion for developing software applications. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+        </div>
     );
 }
+
 export default PortfolioHeader;
