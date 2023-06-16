61de47274eafd100c7e629a00bba33d7f0879e0e
nstuhldreher1 <nbstuhldreher@gmail.com>
2023-06-15 21:21:50 -0400

    Positioned Orbit.

--- my-app/src/components/AboutMe.js
+++ my-app/src/components/AboutMe.js
@@ -1,4 +1,5 @@
 import './AboutMe.css';
+import Orbit from './Orbit.js';
 
 function AboutMe(){
     return(
@@ -7,6 +8,10 @@ function AboutMe(){
                 <h1 id="aboutTitle">About Me</h1>
                 <p id = "aboutDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
+            <div class = "orbit">
+                <Orbit/>
+            </div>
+            
         </section>
     );
 }
