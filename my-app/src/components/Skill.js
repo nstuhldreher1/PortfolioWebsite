c78bcf10f6c0af283ede3897d930d5ecb5ee366c
nstuhldreher1 <nbstuhldreher@gmail.com>
2023-06-23 14:33:01 -0400

    Added logos next to every skill.

--- my-app/src/components/Skill.js
+++ my-app/src/components/Skill.js
@@ -2,7 +2,10 @@ import './Skill.css';
 
 function Skill(prop){
     return(
-        <p id="name">{prop.name}</p>
+        <div id= "one-skill">
+            {console.log(prop.image)}
+            <img src= {prop.image} alt= "" id= "images"></img><p id="name">{prop.name}</p>
+        </div>
     );
 }
 
--- my-app/src/components/Skills.js
+++ my-app/src/components/Skills.js
@@ -1,21 +1,46 @@
 import SkillBlock from './SkillBlock';
-import './Skills.css'
+import './Skills.css';
+import cLogo from './c_24x24.png';
+import cppLogo from './cpp_24x24.png';
+import javaLogo from './java_24x24.png';
+import pythonLogo from './python_24x24.png';
+import jsLogo from './javascript_24x24.png';
+import htmlLogo from './html_24x24.png';
+import cssLogo from './css_24x24.png';
+import reactLogo from './react-color.svg';
+import mongoLogo from './mongodb-color.svg';
+import expressLogo from './express-color.svg';
+import vscodeLogo from './visualstudiocode-color.svg';
+import intellijLogo from './intellijidea-color.svg';
+import pycharmLogo from './pycharm-color.svg';
+import mysqlLogo from './mysql-color.svg';
+import excelLogo from './microsoftexcel-color.svg';
+import ppLogo from './microsoftpowerpoint-color.svg';
+import photoshopLogo from './adobephotoshop-color.svg';
+import indesignLogo from './adobeindesign-color.svg';
+import illustratorLogo from './adobeillustrator-color.svg';
+import figmaLogo from './figma-color.svg';
 
-let props = [
+let blocks = [
     {
         name: "Languages",
-        skills: [ ["image", "C"], ["image","Java"], ["image","Python"], ["image","C#"], ["image","C++"], ["image","JavaScript"], ["image", "CSS"], ["image", "HTML"], ["image", "Haskell"]]
+        skills: [ [cLogo, "C"], [javaLogo,"Java"], [pythonLogo,"Python"], [cppLogo,"C++"], [jsLogo,"JavaScript"], [cssLogo, "CSS"], [htmlLogo, "HTML"]]
     },
     {
         name: "Technologies",
-        skills:[["image","React"], ["image", "MongoDB"], ["image", "ExpressJS"]]
+        skills:[[reactLogo,"React"], [mongoLogo, "MongoDB"], [expressLogo, "ExpressJS"], [vscodeLogo, "Visual Studio Code"], [intellijLogo, "IntelliJ"], [pycharmLogo, "PyCharm"], [mysqlLogo, "MySQL"]]
+    },
+    {
+        name: "Additional Software",
+        skills: [[excelLogo, "Excel"], [ppLogo, "Powerpoint"], [photoshopLogo, "Photoshop"], [indesignLogo, "Indesign"], [illustratorLogo, "Illustrator"], [figmaLogo, "Figma"]]
     }
 ];
 function Skills(){
     return(
         <div id="skills">
-            <SkillBlock name = {props[0].name} skills = {props[0].skills}></SkillBlock>
-            <SkillBlock name = {props[1].name} skills = {props[1].skills}></SkillBlock> 
+            <SkillBlock name = {blocks[0].name} skills = {blocks[0].skills}></SkillBlock>
+            <SkillBlock name = {blocks[1].name} skills = {blocks[1].skills}></SkillBlock> 
+            <SkillBlock name = {blocks[2].name} skills = {blocks[2].skills}></SkillBlock>
         </div>
     );
 }
