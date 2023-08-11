713444602dd2f8ce5a56280e52ba6f724f6edf6d
nstuhldreher1 <nbstuhldreher@gmail.com>
2023-08-11 13:05:05 -0400

    Added the ability to add bullets project overview.

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -3,13 +3,41 @@ import { useParams } from 'react-router-dom';
 
 let projects = [
     {
-        name: "Game Review"
+        name: "Game Review",
+        introduction:"In the dynamic world of video games, where every title holds a unique story and experience, there's a constant desire to share opinions, insights, and recommendations with fellow gamers. Introducing GameReview, a web and mobile application designed exclusively for gaming aficionados. Whether you're a casual player, a dedicated e-sports competitor, or simply someone who appreciates interactive storytelling, GameReview is your one-stop platform for creating, exploring, and engaging with video game reviews.",
+        overview: [
+            {
+                feature: "User Registration and Authentication",
+                description: "Users can easily sign up with their email addresses, and verify their email. Also includes secure authentication for when the user logs in."
+            },
+            {
+                feature: "Game Review Creation",
+                description: "Users can write detailed game reviews for other users to see."
+            },
+            {
+                feature: "Game Review Exploration",
+                description: "Users can browse through a wide variety of reviews that have been created by the community."
+            },
+            {  
+                feature: "User Profile",
+                description: "Every user has their own profile which displays their name, profile picture, and reviews they have written."
+            },
+            {
+                feature: "Game Page",
+                description: "Every game has its own page including a game cover, title, description, average rating, and a list of reviews users have made"
+            }
+        ]
     },
     {   
-        name: "Contact Manager"
+        name: "Contact Manager",
+        introduction: "A contact manager web application",
+        overview: ""
+
     },
     {
-        name: "Portfolio Website"
+        name: "Portfolio Website",
+        introduction: "My portfolio website",
+        overview:""
     }
 ];
 function ProjectPage(){
@@ -18,7 +46,29 @@ function ProjectPage(){
 
     return(
         <div id="project-page">
-            <h1>{projects[name].name}</h1>
+            <p id="project-title">{projects[name].name}</p>
+            <hr/>
+            <p class="project-section">Introduction</p>
+            <p id="introduction">{projects[name].introduction}</p>
+            <hr/>
+            <p class="project-section">Project Overview</p>
+            <ul>
+            {
+                projects[name].overview.map(feature =>{
+                    return(
+                        <li>
+                            <div className='feature-title'>{feature.feature}</div>
+                            <div className ='feature-description'>{feature.description}</div>
+                            <br/>
+                        </li>
+                    );
+                })
+            }
+            </ul>
+            <hr/>
+            <p class="project-section">Technologies Used</p>
+
+
         </div>
     );
 }
