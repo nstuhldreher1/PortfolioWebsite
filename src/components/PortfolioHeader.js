ed8ca1a14425ee03070dff1770ea6e16909d8d8f
nstuhldreher1 <nbstuhldreher@gmail.com>
2023-08-13 08:30:25 -0400

    Added Portfolio introduction.

--- src/components/PortfolioHeader.js
+++ src/components/PortfolioHeader.js
@@ -5,7 +5,7 @@ function PortfolioHeader(){
         <section id= "portheader">
             <div id="header">
                 <h1 id="title">Hey, my name is Nick Stuhldreher.</h1>
-                <p id="description"> Welcome to my portfolio website. I'm a computer science student with a passion for developing software applications. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                <p id="description"> Welcome to my portfolio website. I'm a computer science student with a passion for developing software applications. This portfolio is a showcase of my journey through the world of computer science, highlighting my projects, skills, and accomplishments.  </p>
             </div>
             <div id = "orbit">
                 <Orbit/>
